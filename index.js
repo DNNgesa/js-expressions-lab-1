2f6cb78d21ab6cfe76100acf7a7a513e9a9eee79
Drake Ngesa <61928835+DNNgesa@users.noreply.github.com>
2026-09-24 11:55:50 +0300

    Added total and average temperature calculations

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function convertToCelsiusStrings(data) {
 
 console.log(convertToCelsiusStrings(temperatureData));
 
-// Convert to Fahreheit
+// Convert to Fahrenheit
 function convertToFahrenheitStrings(data) {
   return data.map(item => {
     const fahrenheitValue = item.unit === 'C' 
@@ -43,3 +43,45 @@ function convertToFahrenheitStrings(data) {
 }
 
 console.log(convertToFahrenheitStrings(temperatureData));
+
+// Calculate the total of all temperature values in Fahrenheit
+function calculateTotalInFahrenheit(data) {
+  const total = data.reduce((sum, item) => {
+    
+    const fahrenheitValue = item.unit === 'C' 
+      ? (item.value * 9 / 5) + 32 
+      : item.value;
+      
+    return sum + fahrenheitValue;
+  }, 0);
+
+  return Math.round(total * 100) / 100;
+}
+
+const totalFahrenheit = calculateTotalInFahrenheit(temperatureData);
+console.log(`Total Fahrenheit: ${totalFahrenheit}°F`);
+
+ // Calculate the total of all temperature values in Celsius
+function calculateTotalInCelsius(data) {
+  const total = data.reduce((sum, item) => {
+    
+    const celsiusValue = item.unit === 'F' 
+      ? (item.value - 32) * 5 / 9 
+      : item.value;
+      
+    return sum + celsiusValue;
+  }, 0);
+
+  return Math.round(total * 100) / 100;
+}
+
+const totalCelsius = calculateTotalInCelsius(temperatureData);
+console.log(`Total Celsius: ${totalCelsius}°C`);
+
+
+ // Average Temperatures
+const averageFahrenheit = totalFahrenheit/30;
+console.log(`Average Fahrenheit: ${averageFahrenheit}°F`);
+
+const averageCelsius =  totalCelsius/30
+console.log(`Average Celsius: ${averageCelsius}°C`);
