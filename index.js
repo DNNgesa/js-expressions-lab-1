3300ba34e24ffabd4950352d0efa079609eedcb1
Drake Ngesa <61928835+DNNgesa@users.noreply.github.com>
2026-09-24 14:49:49 +0300

    Rename functions for temperature calculations

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function convertToCelsiusStrings(data) {
 
 console.log(convertToCelsiusStrings(temperatureData));
 
-// Convert to Fahrenheit
+// Convert to Fahreheit
 function convertToFahrenheitStrings(data) {
   return data.map(item => {
     const fahrenheitValue = item.unit === 'C' 
@@ -44,8 +44,9 @@ function convertToFahrenheitStrings(data) {
 
 console.log(convertToFahrenheitStrings(temperatureData));
 
+
 // Calculate the total of all temperature values in Fahrenheit
-function calculateTotalInFahrenheit(data) {
+function tot_temperature_in_fahrenheit(data) {
   const total = data.reduce((sum, item) => {
     
     const fahrenheitValue = item.unit === 'C' 
@@ -58,11 +59,11 @@ function calculateTotalInFahrenheit(data) {
   return Math.round(total * 100) / 100;
 }
 
-const totalFahrenheit = calculateTotalInFahrenheit(temperatureData);
+const totalFahrenheit = tot_temperature_in_fahrenheit(temperatureData);
 console.log(`Total Fahrenheit: ${totalFahrenheit}°F`);
 
  // Calculate the total of all temperature values in Celsius
-function calculateTotalInCelsius(data) {
+function tot_temperature_in_celsius(data) {
   const total = data.reduce((sum, item) => {
     
     const celsiusValue = item.unit === 'F' 
@@ -75,13 +76,20 @@ function calculateTotalInCelsius(data) {
   return Math.round(total * 100) / 100;
 }
 
-const totalCelsius = calculateTotalInCelsius(temperatureData);
+const totalCelsius = tot_temperature_in_celsius(temperatureData);
 console.log(`Total Celsius: ${totalCelsius}°C`);
 
 
  // Average Temperatures
-const averageFahrenheit = totalFahrenheit/30;
-console.log(`Average Fahrenheit: ${averageFahrenheit}°F`);
+const avg_temperature_in_fahrenheit = tot_temperature_in_fahrenheit(temperatureData)/30;
+console.log(`Average Fahrenheit: ${avg_temperature_in_fahrenheit}°F`);
 
-const averageCelsius =  totalCelsius/30
-console.log(`Average Celsius: ${averageCelsius}°C`);
+const avg_temperature_in_celsius =  tot_temperature_in_celsius(temperatureData)/30
+console.log(`Average Celsius: ${avg_temperature_in_celsius}°C`);
+
+module.exports = {
+  // tot_temperature_in_fahrenheit,
+  // tot_temperature_in_celsius,
+  // avg_temperature_in_fahrenheit,
+  // avg_temperature_in_celsius
+};
